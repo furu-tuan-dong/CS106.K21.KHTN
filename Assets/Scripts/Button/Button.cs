0a211949fa94fde2d8f9cb7541bcef8308a89284
furu-tuan-dong <trankhanhvy3008@gmail.com>
2020-05-26 13:27:20 +0700

    source mummy maze game

---
+++ Assets/Scripts/Button/Button.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Sprite texture;
+    public Sprite hoverTexture;
+    public Sprite downTexture;
+    SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        render.sprite = texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter() {
+        if (render.sprite == texture)
+            render.sprite = hoverTexture;
+    }
+
+    void OnMouseExit() {
+        if (render.sprite == hoverTexture)
+            render.sprite = texture;
+    }
+
+    void OnMouseDown() {
+        if (downTexture != null)
+            render.sprite = downTexture;
+    }
+
+    void OnMouseUp() {
+        if (downTexture != null)
+            render.sprite = texture;
+    }
+}
