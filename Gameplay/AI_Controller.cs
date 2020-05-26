7a079f60aaa22da1511b35a193ebde207cb062ed
hmtrii <42197959+hmtrii@users.noreply.github.com>
2020-05-26 12:57:00 +0700

    First

---
+++ Gameplay/AI_Controller.cs
@@ -0,0 +1,306 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AI_Controller : MonoBehaviour
+{
+    class Checkpoint {
+        public Vector3 player;
+        public List<Vector3> mummies = new List<Vector3>();
+    }
+
+    
+    // Inspector
+    public GameObject winOverlay;
+    public GameObject loseOverlay;
+    public GameObject dust_effect;
+    public GameObject defeat_effect;
+    
+    // Internal
+    public bool idle;
+    public Character player;
+    public List<Character> mummies;
+    public Stack checkpoints = new Stack();
+    
+    // Static
+    public int size;
+    int[,] verticalWall;
+    int[,] horizontalWall;
+    Vector3 stairPosition;
+    Vector3 stairDirection;
+    bool restrictedVision = false;
+
+
+    List<Vector3[]> actions = new List<Vector3[]>();
+
+
+    void Awake() {
+        size = 6;
+        idle = true;
+        mummies = new List<Character>();
+        verticalWall = new int[size, size];
+        horizontalWall = new int[size, size];
+    }
+
+    GameState state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int n = size;
+        foreach (Transform t in transform) {
+            int x = (int) t.localPosition.x;
+            int y = (int) t.localPosition.y;
+
+            switch (t.tag) {
+                case "Player":
+                    player = t.GetComponent<Character>();
+                    break;
+                case "WhiteMummy":
+                case "RedMummy":
+                    mummies.Add(t.GetComponent<Character>());
+                    break;
+                case "Stair":
+                    stairPosition = t.localPosition;
+                    if (x == 0) stairDirection = Vector3.left;
+                    if (y == 0) stairDirection = Vector3.down;
+                    if (x == n) {
+                        stairPosition.x--;
+                        stairDirection = Vector3.right;
+                    }
+                    if (y == n) {
+                        stairPosition.y--;
+                        stairDirection = Vector3.up;
+                    }
+                    break;
+                case "VerticalWall":
+                    verticalWall[x, y] = 1;
+                    break;
+                case "HorizontalWall":
+                    horizontalWall[x, y] = 1;
+                    break;
+                default:
+                    Debug.Log("Unexpected game object with tag: " + t.tag);
+                    break;
+            }
+        }
+
+
+        //List<Vector3> mummiesPos = new List<Vector3>();
+        //foreach(Character mummy in mummies)
+        //{
+        //    mummiesPos.Add(mummy.transform.localPosition);
+        //}
+        state = new GameState(player, mummies, size, verticalWall, horizontalWall, stairPosition);
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (!idle)
+        {
+            return;
+        }
+        
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKeyDown("up")) direction = Vector3.up;
+        else if (Input.GetKeyDown("down")) direction = Vector3.down;
+        else if (Input.GetKeyDown("left")) direction = Vector3.left;
+        else if (Input.GetKeyDown("right")) direction = Vector3.right;
+
+
+        //direction = actions[i]
+        
+
+        if (direction != Vector3.zero)
+        {
+            StartCoroutine(Action(direction));
+        }
+
+        
+
+        //al.print(idle);
+    }
+
+    public IEnumerator Action(Vector3 direction) {
+
+        // Player move 1 step
+        if (Blocked(player.transform.localPosition, direction)) yield break;
+        actions = state.Action(direction);
+
+        idle = false;
+        yield return player.Move(actions[0][1], false);
+
+        yield return MummiesMove(actions);
+
+        if (MummiesCatch())
+        {
+            yield return Lost();
+            yield break;
+        }
+
+        yield return MummiesFight();
+        
+
+        if (player.transform.localPosition == stairPosition)
+        {
+            yield return Victory();
+            yield break;
+        }
+
+        idle = true;
+    }
+
+    // Win and lose
+    IEnumerator Victory() {
+        yield return player.Move(stairDirection, false);
+
+        Destroy(player.gameObject);
+        foreach (var mummy in mummies) 
+            Destroy(mummy.gameObject);
+
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(winOverlay, transform, true);
+
+    }
+
+    IEnumerator Lost() {
+        Vector3 position = player.transform.localPosition;
+
+        Destroy(player.gameObject);
+        foreach (var mummy in mummies) 
+            Destroy(mummy.gameObject);
+
+        yield return RunEffect(defeat_effect, position, false);
+        yield return RunEffect(dust_effect, position, true);
+
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(loseOverlay, transform, true);
+    }
+
+    IEnumerator MummiesMove(List<Vector3[]> actions) {
+        List<IEnumerator> coroutines = new List<IEnumerator>();
+
+        foreach (var mummy in mummies) 
+        {
+            Vector3 nextMove = Vector3.zero;
+            foreach(Vector3[] posAct in actions)
+            {
+                if(mummy.transform.localPosition == posAct[0])
+                {
+                    nextMove = posAct[1];
+                }
+            }
+            //Vector3 next_move = mummy.tag == "WhiteMummy"
+            //    ? state.getWhiteTrace(mummy.transform.localPosition)
+            //    : state.getRedTrace(mummy.transform.localPosition);
+            
+            bool isBlocked = Blocked(mummy.transform.localPosition, nextMove);
+            
+            coroutines.Add(mummy.Move(nextMove, isBlocked));
+        }
+
+        yield return PromiseAll(coroutines.ToArray());
+    }
+
+    bool MummiesCatch() {
+        foreach (var mummy in mummies) {
+            if (mummy.transform.localPosition == player.transform.localPosition)
+                return true;
+        }
+        return false;
+    }
+
+    IEnumerator MummiesFight() {
+        // Group mummy by position
+        var positions = new Dictionary<Vector3, List<Character>>();
+
+        foreach (var mummy in mummies) {
+            Vector3 key = mummy.transform.localPosition;
+            if (!positions.ContainsKey(key))
+                positions.Add(key, new List<Character>());
+            
+            positions[key].Add(mummy);
+        }
+
+        //Delete mummy and run effect
+        var effects = new List<IEnumerator>();
+        
+        foreach (var item in positions) {
+            if (item.Value.Count == 1) continue;
+
+            // Preserve one
+            item.Value.RemoveAt(0);
+            foreach (var mummy in item.Value) {
+                mummies.Remove(mummy);
+                Destroy(mummy.gameObject);
+            }
+            
+            effects.Add(RunEffect(dust_effect, item.Key, true));
+        }
+
+        yield return PromiseAll(effects.ToArray());
+    }
+
+    // Character vs walls
+    public bool Blocked(Vector3 position, Vector3 direction) {
+        int x = (int)position.x;
+        int y = (int)position.y;
+        int n = size-1;
+        
+        if (direction == Vector3.up)
+            return y == n || horizontalWall[x, y+1] == 1;
+
+        if (direction == Vector3.down)
+            return y == 0 || horizontalWall[x, y] == 1;
+
+        if (direction == Vector3.left)
+            return x == 0 || verticalWall[x, y] == 1;
+        
+        if (direction == Vector3.right)
+            return x == n || verticalWall[x+1, y] == 1;
+        
+        return true;
+    }
+
+
+    // Helper functions
+    IEnumerator RunEffect(GameObject effect, Vector3 position, bool clear) {
+        GameObject fx = Instantiate(effect, transform);
+        fx.transform.localPosition = position;
+        yield return fx.GetComponent<Effect>().Run(clear);
+    }
+    
+    IEnumerator PromiseAll(params IEnumerator[] coroutines) {
+        while (true) {
+            object current = null;
+
+            foreach (IEnumerator x in coroutines) {
+                if (x.MoveNext() == true)
+                    current = x.Current;
+            }
+
+            if (current == null) break;
+            yield return current;
+        }
+    }
+
+    // Undo
+    public void Save() {
+        var checkpoint = new Checkpoint();
+        checkpoint.player = player.transform.localPosition;
+        foreach (var mummy in mummies) {
+            checkpoint.mummies.Add(mummy.transform.localPosition);
+        }
+        checkpoints.Push(checkpoint);
+    }
+
+    public void Restore() {
+        var checkpoint = (Checkpoint)checkpoints.Pop();
+        
+    }
+}
---
+++ Gameplay/Algorithm.cs
@@ -0,0 +1,435 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+//using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class GameState
+{
+    private Character player;
+    private Vector3 playerPos;
+    private List<Character> mummies;
+    private List<Vector3> mummiesPos = new List<Vector3>();
+    private int size;
+    private int[,] verticalWall;
+    private int[,] horizontalWall;
+    private Vector3 stairPosition;
+    //private bool get_pos = true;
+    AI_Controller control = new AI_Controller();
+    int numMummies;
+
+    public GameState(Character player, List<Character> mummies, int size,
+        int[,] verticalWall, int[,] horizontalWall, Vector3 stairPosition)
+    {
+        this.player = player;
+        playerPos = player.transform.localPosition;
+        this.mummies = mummies;
+        numMummies = mummies.Count;
+        this.size = size;
+        this.verticalWall = verticalWall;
+        this.horizontalWall = horizontalWall;
+        this.stairPosition = stairPosition;
+
+        //mummyAndAction = new Vector3[mummies.Count, 2];
+        //for(int i = 0; i < numMummies; i++)
+        //{
+        //    mummyAndAction[i, 0] = mummies[i].transform.localPosition;
+        //}
+
+
+        foreach(Character mummy in mummies)
+        {
+            mummiesPos.Add(mummy.transform.localPosition);
+        }
+    }
+
+    public List<Vector3[]> Action(Vector3 direction)
+    {
+        List<Vector3[]> result = new List<Vector3[]>();
+        if (!isBlocked(direction, playerPos))
+        {
+            //Player
+            Vector3[] playerResult = new Vector3[] { playerPos, direction };
+            result.Add(playerResult);
+            Debug.Log(result[0][1]);
+            Debug.Log(result[0][0]);
+            playerPos += direction;
+
+            //Update mummies's position.
+            MummiesMove(result);
+
+        }
+
+        //for(int i = 0;i < result.Count; i++)
+        //{
+        //    Debug.Log(result[i][0]);
+        //}
+        
+
+        return result;
+    }
+
+    void MummiesMove(List<Vector3[]> result)
+    {
+        for(int i = 0; i < numMummies; i++)
+        {
+            Vector3 nextMove;
+            Vector3[] resultMummy;
+            if(mummies[i].tag == "WhiteMummy")
+            {
+                nextMove = WhiteTrace(mummiesPos[i]);
+                if(isBlocked(nextMove, mummiesPos[i]))
+                {
+                    return;
+                }
+                resultMummy = new Vector3[] { mummiesPos[i], nextMove };
+                result.Add(resultMummy);
+            }
+            else
+            {
+                nextMove = RedTrace(mummiesPos[i]);
+                if (isBlocked(nextMove, mummiesPos[i]))
+                {
+                    return;
+                }
+                resultMummy = new Vector3[] { mummiesPos[i], nextMove };
+                result.Add(resultMummy);
+            }
+
+            //mummyAndAction[i, 0] += nextMove;
+            mummiesPos[i] += nextMove;
+        }
+    }
+    
+    Vector3 WhiteTrace(Vector3 position)
+    {
+        AstarSearch actions = new AstarSearch(this, position);
+        Vector3 getAction = actions.GetActions();
+        //for (int i = 0; i< numMummies; i++)
+        //{
+        //    if(mummiesPos[i] == position)
+        //    {
+        //        mummiesPos[i] = getAction;
+        //    }
+        //}
+        return getAction;
+    }
+
+    Vector3 RedTrace(Vector3 position)
+    {
+        AstarSearch actions = new AstarSearch(this, position);
+        Vector3 getAction = actions.GetActions();
+        //for (int i = 0; i < numMummies; i++)
+        //{
+        //    if (mummiesPos[i] == position)
+        //    {
+        //        mummiesPos[i] = getAction;
+        //    }
+        //}
+        return getAction;
+    }
+
+    //public Vector3 getWhiteTrace(Vector3 position)
+    //{
+    //    for(int i = 0; i < numMummies; i++)
+    //    {
+    //        if(mummyAndAction[i, 0] == position)
+    //        { 
+    //            return mummyAndAction[i, 1];
+    //        }
+    //    }
+    //    return Vector3.zero;
+    //}
+
+    //public Vector3 getRedTrace(Vector3 position)
+    //{
+    //    for (int i = 0; i < numMummies; i++)
+    //    {
+    //        if (mummyAndAction[i, 0] == position)
+    //        {
+    //            return mummyAndAction[i, 1];
+    //        }
+    //    }
+    //    return Vector3.zero;
+    //}
+    
+    public bool isBlocked(Vector3 direction, Vector3 position)
+    {
+        int x = (int)position.x;
+        int y = (int)position.y;
+        int n = size - 1;
+
+        if (direction == Vector3.up)
+            return y == n || horizontalWall[x, y + 1] == 1;
+
+        if (direction == Vector3.down)
+            return y == 0 || horizontalWall[x, y] == 1;
+
+        if (direction == Vector3.left)
+            return x == 0 || verticalWall[x, y] == 1;
+
+        if (direction == Vector3.right)
+            return x == n || verticalWall[x + 1, y] == 1;
+
+        return true;
+    }
+
+
+    //public List<Vector3> GetSuccessors(Vector3 position)
+    //{
+    //    List<Vector3> successors = new List<Vector3>();
+    //    Vector3[] directions = new Vector3[] { Vector3.left, Vector3.up, Vector3.right, Vector3.down };
+    //    foreach(Vector3 direction in directions)
+    //    {
+    //        if(!isBlocked(direction, position))
+    //        {
+    //            successors.Add(position + direction);
+    //        }
+    //    }
+
+    //    return successors;
+    //}
+    
+    public Vector3 GetPlayerPosition()
+    {
+        return playerPos;
+    }
+
+    //public void print(bool idle)
+    //{
+    //    if (idle == true && get_pos == true)
+    //    {
+    //        get_pos = false;
+    //        Debug.Log(player.transform.localPosition);
+    //        foreach (Character m in mummies)
+    //        {
+    //            Debug.Log(m.transform.localPosition);
+    //        }
+    //        return;
+
+    //    }
+    //    if (idle == false)
+    //    {
+    //        get_pos = true;
+    //    }
+
+    //    //Debug.Log(verticalWall);
+    //    //Debug.Log(horizontalWall[3, 2]);
+    //}
+
+
+    
+    //public Vector3 getAction()
+    //{
+    //    Agent a = expectimax(2);
+    //    return a.getAction();
+    //}
+
+
+    //int d = 0;
+    //public Agent expectimax(int depth)
+    //{
+    //    Agent currentAgent = new Agent();
+    //    Vector3[] directions = new Vector3[] { Vector3.left, Vector3.up, Vector3.right, Vector3.down };
+
+    //    //Check termination.
+    //    if(d == depth)
+    //    {
+    //        currentAgent.set(Vector3.zero, evaluationFunction());
+            
+    //        return currentAgent;
+    //    }
+
+    //    foreach(Vector3 direction in directions)
+    //    {
+    //        Agent value;
+    //        control.Action(direction);
+    //        value = expectimax(d);
+    //        //The first action, agent is not assigned direction and value
+    //        if (currentAgent.getValue() == -1)
+    //        {
+    //            currentAgent.set(direction, value.getValue());
+    //        }
+    //        else
+    //        {
+    //            if (value.getValue() > currentAgent.getValue())
+    //            {
+    //                currentAgent.set(direction, value.getValue());
+    //            }
+    //        }
+    //    }
+    //    d += 1;
+
+    //    return currentAgent;
+    //}
+
+    
+
+    
+}
+
+public class Node
+{
+    Vector3 position;
+    List<Vector3> tracePath;
+    int value;
+    
+    public Node(Vector3 position)
+    {
+        this.position = position;
+        tracePath = new List<Vector3>();
+        value = 0;
+    }
+
+    public Vector3 getPosition()
+    {
+        return position;
+    }
+
+    public Vector3 getFirstDirection()
+    {
+        return tracePath[0];
+    }
+
+    public void updateValue(int heuristicValue)
+    {
+        value = heuristicValue + tracePath.Count;
+    }
+
+    public void setTracePath(List<Vector3> preTracePath, Vector3 direction)
+    {
+        tracePath = preTracePath;
+        tracePath.Add(direction);
+    }
+
+    public List<Vector3> getTracePath()
+    {
+        return tracePath;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
+
+public class AstarSearch
+{
+    GameState state;
+    Vector3 startedPosition;
+    public AstarSearch(GameState state, Vector3 startedPosition)
+    {
+        this.state = state; 
+        this.startedPosition = startedPosition;
+    }
+    
+    public Vector3 GetActions()
+    {
+        //Node start = new Node(startedPosition);
+        //List<Node> frontiers = new List<Node>();
+        //List<Vector3> closed = new List<Vector3>();
+        //frontiers.Add(start);
+        //closed.Add(start.getPosition());
+
+        //if (isCatch(state, start.getPosition()))
+        //{
+        //    return Vector3.zero;
+        //    //return start.getFirstDirection();
+        //}
+
+
+        //while (frontiers.Count != 0)
+        //{
+        //    Node currentNode = frontiers[0];
+        //    frontiers.RemoveAt(0);
+        //    Vector3 currentPos = currentNode.getPosition();
+        //    List<Vector3> currentTracePath = currentNode.getTracePath();
+
+        //    if (isCatch(state, currentPos))
+        //    {
+        //        return currentNode.getFirstDirection();
+        //    }
+
+        //    closed.Add(currentPos);
+
+        //    Vector3[] directions = new Vector3[] { Vector3.left, Vector3.up, Vector3.right, Vector3.down };
+        //    foreach (Vector3 direction in directions)
+        //    {
+
+        //        if (!state.isBlocked(direction, currentPos))
+        //        {
+        //            Node newNode = new Node(currentPos + direction);
+        //            newNode.setTracePath(currentTracePath, direction);
+        //            newNode.updateValue(Heuristic(state, newNode.getPosition()));
+
+        //            if (!closed.Contains(newNode.getPosition()))
+        //            {
+        //                update(frontiers, newNode);
+        //            }
+        //        }
+        //    }
+        //}
+
+        //return Vector3.zero;
+        return Vector3.right;
+    }
+
+    void update(List<Node> list, Node insertedNode)
+    {
+        foreach(Node n in list)
+        {
+            if(n.getPosition() == insertedNode.getPosition())
+            {
+                if(n.getValue() <= insertedNode.getValue())
+                {
+                    return;
+                }
+                else
+                {
+                    list.Remove(n);
+                    break;
+                }
+            }
+        }
+
+        if(list.Count == 0)
+        {
+            list.Add(insertedNode);
+        }
+        else
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(insertedNode.getValue() <= list[i].getValue())
+                {
+                    list.Insert(i, insertedNode);
+                }
+            }
+        }
+        
+        list.Add(insertedNode);
+    }
+
+    bool isCatch(GameState state, Vector3 position)
+    {
+        if (position == state.GetPlayerPosition())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int Heuristic(GameState state, Vector3 position)
+    {
+        int disManhatan = (int)(Mathf.Abs(position.x - state.GetPlayerPosition().x) + Mathf.Abs(position.y - state.GetPlayerPosition().y));
+        return disManhatan;
+    }
+}
