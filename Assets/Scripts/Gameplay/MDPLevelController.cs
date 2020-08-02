aefdb003af3d6376b6d6b8f3eaa6506ba56ba6b8
DatDoc <62165465+DatDoc@users.noreply.github.com>
2020-08-02 09:16:45 +0700

    Update MDPLevelController.cs

--- Assets/Scripts/Gameplay/MDPLevelController.cs
+++ Assets/Scripts/Gameplay/MDPLevelController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
@@ -11,13 +11,13 @@ public class MDPLevelController : MonoBehaviour
     public GameObject winOverlay;
     public GameObject loseOverlay;
     public GameObject dust_effect;
-
+    
     // Internal
-    public double probability = 0.4;
+    public double probability = 0.5;
     public bool idle;
     public Character player;
-
-
+    
+    
     // Static
     public int size;
     int[,] hole;
@@ -29,11 +29,10 @@ public class MDPLevelController : MonoBehaviour
 
 
     // value iteration
-    int observation_space = 36;
-    int action_space = 4;
-
-    void Awake()
-    {
+    public int observation_space = 36;
+    public int action_space = 4;
+    
+    void Awake() {
         size = 6;
         idle = true;
         probability = Math.Min(1.0, probability);
@@ -46,13 +45,11 @@ public class MDPLevelController : MonoBehaviour
     void Start()
     {
         int n = size;
-        foreach (Transform t in transform)
-        {
-            int x = (int)t.localPosition.x;
-            int y = (int)t.localPosition.y;
+        foreach (Transform t in transform) {
+            int x = (int) t.localPosition.x;
+            int y = (int) t.localPosition.y;
 
-            switch (t.tag)
-            {
+            switch (t.tag) {
                 case "Player":
                     player = t.GetComponent<Character>();
                     break;
@@ -60,13 +57,11 @@ public class MDPLevelController : MonoBehaviour
                     stairPosition = t.localPosition;
                     if (x == 0) stairDirection = Vector3.left;
                     if (y == 0) stairDirection = Vector3.down;
-                    if (x == n)
-                    {
+                    if (x == n) {
                         stairPosition.x--;
                         stairDirection = Vector3.right;
                     }
-                    if (y == n)
-                    {
+                    if (y == n) {
                         stairPosition.y--;
                         stairDirection = Vector3.up;
                     }
@@ -88,18 +83,20 @@ public class MDPLevelController : MonoBehaviour
     }
 
 
-    Array Value_iteration(int max_iter, double gamma = 0.9)
+    Array Value_iteration(int max_iter, ArrayList hole_list, int stair_pos, double gamma = 0.9)
     {
-
-        double[] v_values = new double[observation_space];
+        
+        double[] v_values = new double[observation_space]; 
         Array.Clear(v_values, 0, v_values.Length); // set the v_values array to 0
 
         for (int i = 0; i < max_iter; i++)
         {
             double[] pre_v_values = new double[observation_space];
             Array.Copy(v_values, 0, pre_v_values, 0, observation_space);
-            for (int state = 0; state < observation_space; state++)
+            for (int state = 0; state < observation_space; state++) //check 36 states
             {
+
+                //get current position of the explorer
                 Vector3 cur_pos = new Vector3
                 {
                     x = state % 6
@@ -112,32 +109,34 @@ public class MDPLevelController : MonoBehaviour
                 else if (state >= 0 && state <= 5) cur_pos.y = 5;
 
 
+
                 double[] q_values = new double[action_space];
                 Array.Clear(q_values, 0, q_values.Length);
                 double q_value = 0.0;
-                if (state == 5 || state == 12 || state == 35 || state == 0)
+
+                if (hole_list.Contains(state) || state == stair_pos) // if state is in hole_list
                 {
-                    q_value = 1.0 * (0.0 + gamma * pre_v_values[state]);
+                    q_value = 1.0 * (0.0 + gamma * pre_v_values[state]); // so the game end
                     q_values[0] = q_value;
                     for (int tmp = 1; tmp < 4; tmp++) q_values[tmp] = 0;
                 }
                 else
                 {
-                    for (int action = 0; action < action_space; action++)
+                    for (int action = 0; action < action_space; action++) // check 4 directions
                     {
                         q_value = 0.0;
-                        int[] possible_actions = new int[3];
+                        int[] possible_actions = new int[3]; // each direction got 3 possible actions
 
                         /// 0 : UP
                         /// 1 : RIGHT
                         /// 2 : DOWN
                         /// 3 : LEFT
-                        if (action == 0) { possible_actions[0] = 0; possible_actions[1] = 3; possible_actions[2] = 1; }
-                        if (action == 1) { possible_actions[0] = 1; possible_actions[1] = 0; possible_actions[2] = 2; }
-                        if (action == 2) { possible_actions[0] = 2; possible_actions[1] = 1; possible_actions[2] = 3; }
-                        if (action == 3) { possible_actions[0] = 3; possible_actions[1] = 2; possible_actions[2] = 0; }
+                        if (action == 0) { possible_actions[0] = 0; possible_actions[1] = 3; possible_actions[2] = 1; } //UP : 3 possible actions are LEFT, UP, RIGHT
+                        if (action == 1) { possible_actions[0] = 1; possible_actions[1] = 0; possible_actions[2] = 2; } //RIGHT : 3 possible actions are RIGHT, UP, DOWN
+                        if (action == 2) { possible_actions[0] = 2; possible_actions[1] = 1; possible_actions[2] = 3; } //DOWN : 3 possible actions are LEFT, DOWN, RIGHT
+                        if (action == 3) { possible_actions[0] = 3; possible_actions[1] = 2; possible_actions[2] = 0; } //LEFT : 3 possible actions are LEFT, UP, DOWN
 
-                        for (int possible_action = 0; possible_action < possible_actions.Length; possible_action++)
+                        for (int possible_action = 0; possible_action < possible_actions.Length; possible_action++) // check 3 possible action and set the next state
                         {
                             int next_state = 0;
                             switch (possible_actions[possible_action])
@@ -170,46 +169,46 @@ public class MDPLevelController : MonoBehaviour
                             }
 
                             double reward = 0.0;
-                            if (next_state == 5 || next_state == 12 || next_state == 35)
-                                reward = -1.0;
-                            else
-                            {
-                                if (next_state == 0) reward = 1.0;
-                            }
+                            if (hole_list.Contains(next_state)) reward = -1.0;
+                            else if (next_state == stair_pos) reward = 1.0;
 
                             if (possible_action == 0)
-                                q_value += 0.4 * (reward + gamma * pre_v_values[next_state]);
+                                q_value += probability * (reward + gamma * pre_v_values[next_state]);
 
                             else
-                                q_value += 0.3 * (reward + gamma * pre_v_values[next_state]);
-
+                                q_value += ((1.0 - probability) / 2) * (reward + gamma * pre_v_values[next_state]);
                         }
                         q_values[action] = q_value;
 
                     }
                 }
-                //int best_action = Array.IndexOf(q_values, q_values.Max());
                 v_values[state] = q_values.Max();
 
             }
+            
             if (Enumerable.SequenceEqual(v_values, pre_v_values))
                 break;
-
+            
         }
-
+        //for (int i = 0; i < 36; i++)
+        //{
+        //    Debug.Log(i + 1 + ": " + v_values[i]);
+        //}
         return v_values;
     }
 
 
-    Array Policy_extraction(double[] v_values, double gamma = 0.9)
+    Array Policy_extraction(double []v_values, ArrayList hole_list, int stair_pos,double gamma = 0.9)
     {
+        
         Vector3[] policy = new Vector3[observation_space];
         Array.Clear(policy, 0, policy.Length); // set the v_values array to 0
         for (int state = 0; state < observation_space; state++)
         {
             double[] q_values = new double[action_space];
             double q_value = 0.0;
-            /// get current position
+
+            /// get current position of the explorer
             Vector3 cur_pos = new Vector3
             {
                 x = state % 6
@@ -220,8 +219,9 @@ public class MDPLevelController : MonoBehaviour
             else if (state >= 12 && state <= 17) cur_pos.y = 3;
             else if (state >= 6 && state <= 11) cur_pos.y = 4;
             else if (state >= 0 && state <= 5) cur_pos.y = 5;
-            ///
-            if (state == 5 || state == 12 || state == 35 || state == 0)
+
+
+            if (hole_list.Contains(state) || state == stair_pos)
             {
                 q_value = 1.0 * (0.0 + gamma * v_values[state]);
                 q_values[0] = q_value;
@@ -276,26 +276,23 @@ public class MDPLevelController : MonoBehaviour
                         }
 
                         double reward = 0.0;
-                        if (next_state == 5 || next_state == 12 || next_state == 35)
-                            reward = -1.0;
-                        else
-                        {
-                            if (next_state == 0) reward = 1.0;
-                        }
+                        if (hole_list.Contains(next_state)) reward = -1.0;
+                        else if (next_state == stair_pos) reward = 1.0;
 
                         if (possible_action == 0)
-                            q_value += 0.4 * (reward + gamma * v_values[next_state]);
+                            q_value += probability * (reward + gamma * v_values[next_state]);
 
                         else
-                            q_value += 0.3 * (reward + gamma * v_values[next_state]);
+                            q_value += ((1.0 - probability) / 2) * (reward + gamma * v_values[next_state]);
 
                     }
                     q_values[action] = q_value;
 
                 }
             }
+            
             int best_action = Array.IndexOf(q_values, q_values.Max());
-            switch (best_action)
+            switch(best_action)
             {
                 case 0:
                     policy[state] = Vector3.up;
@@ -311,7 +308,7 @@ public class MDPLevelController : MonoBehaviour
                     break;
             }
         }
-
+        
         return policy;
     }
 
@@ -321,24 +318,41 @@ public class MDPLevelController : MonoBehaviour
         if (!idle) return;
         Vector3 direction = Vector3.zero;
 
-        //if (Input.GetKeyDown("up")) direction = Vector3.up;
-        //else if (Input.GetKeyDown("down")) direction = Vector3.down;
-        //else if (Input.GetKeyDown("left")) direction = Vector3.left;
-        //else if (Input.GetKeyDown("right")) direction = Vector3.right;
+  
+        var hole_list = new ArrayList();
+        int stair_pos = 0;
+        for (int x = 0; x < 6; x++)
+        {
+            int pos = 0;
+            for (int y = 0; y < 6; y++)
+            {
+                if (y == 0) pos = 30 + x;
+                else if (y == 1) pos = 24 + x;
+                else if (y == 2) pos = 18 + x;
+                else if (y == 3) pos = 12 + x;
+                else if (y == 4) pos = 6 + x;
+                else if (y == 5) pos = x;
 
+                if (hole[x, y] == 1) hole_list.Add(pos);
+                else if (x == stairPosition.x && y == stairPosition.y) stair_pos = pos;
+            }
+        }
 
         double[] value_iter = new double[observation_space];
-        Array.Copy(Value_iteration(1000), 0, value_iter, 0, observation_space);
 
+        //max_iter = 1000
+        Array.Copy(Value_iteration(1000, hole_list, stair_pos, 0.9), 0, value_iter, 0, observation_space);
+
+        
         Vector3[] policy_iter = new Vector3[observation_space];
-        Array.Copy(Policy_extraction(value_iter), 0, policy_iter, 0, observation_space);
-
-
+        Array.Copy(Policy_extraction(value_iter, hole_list, stair_pos, 0.9), 0, policy_iter, 0, observation_space);
+        
+        // get player position
         int player_position = 0;
         int x_pos = (int)player.transform.localPosition.x;
         switch (player.transform.localPosition.y)
         {
-            case 0:
+            case 0: 
                 player_position = 30 + x_pos;
                 break;
             case 1:
@@ -359,21 +373,18 @@ public class MDPLevelController : MonoBehaviour
         }
 
         direction = policy_iter[player_position];
-
         direction = getDirectionForAgent(direction, probability);
-
 
         if (direction != Vector3.zero)
             StartCoroutine(Action(direction));
     }
 
+    
 
-
-    Vector3 getDirectionForAgent(Vector3 inputDirection, double probForInput)
-    {
+    Vector3 getDirectionForAgent(Vector3 inputDirection, double probForInput){
         if (inputDirection == Vector3.zero) return inputDirection;
         //Vector3 returnDirection;
-        List<Vector3> allDirection = new List<Vector3>() { Vector3.up, Vector3.down, Vector3.right, Vector3.left };
+        List<Vector3> allDirection = new List<Vector3>(){Vector3.up, Vector3.down, Vector3.right, Vector3.left};
         double probForOther = (1 - probForInput) / 2;
         System.Random randomObj = new System.Random();
         double probRandom = randomObj.NextDouble();
@@ -384,13 +395,14 @@ public class MDPLevelController : MonoBehaviour
         if (inputDirection == Vector3.down) { possible_actions[0] = Vector3.down; possible_actions[1] = Vector3.right; possible_actions[2] = Vector3.left; }
         if (inputDirection == Vector3.left) { possible_actions[0] = Vector3.left; possible_actions[1] = Vector3.down; possible_actions[2] = Vector3.up; }
 
-        if (probRandom <= 0.8)
+        
+        if (probRandom <= probability)
         {
             return possible_actions[0];
         }
         else
         {
-            if (probRandom > 0.8 || probRandom <= 0.9)
+            if ((probRandom > probability) && (probRandom <= (probability + (1.0 - probability) / 2)))
             {
                 return possible_actions[1];
             }
@@ -400,39 +412,26 @@ public class MDPLevelController : MonoBehaviour
             }
         }
 
-        //double minProb = Math.Min(probForInput, probForOther);
-        //bool isInputMinProb = minProb == probForInput;
-        //if ((probRandom <= minProb && isInputMinProb) || (probRandom > minProb && !isInputMinProb)){
-        //    return inputDirection;
-        //}
-        //do{
-        //    returnDirection = allDirection[randomObj.Next(0, 3)];
-        //} while(returnDirection == inputDirection);
-        //return returnDirection;
     }
-    IEnumerator Action(Vector3 direction)
-    {
+    IEnumerator Action(Vector3 direction) {
 
         // Player move 1 step
         if (Blocked(player.transform.localPosition, direction)) yield break;
 
         idle = false;
         yield return player.Move(direction, false);
-        if (FallHoles())
-        {
+        if (FallHoles()){
             yield return Lost();
             yield break;
         }
-        if (player.transform.localPosition == stairPosition)
-        {
+        if (player.transform.localPosition == stairPosition) {
             yield return Victory();
             yield break;
         }
-
+        
         idle = true;
     }
-    IEnumerator Victory()
-    {
+    IEnumerator Victory() {
         yield return player.Move(stairDirection, false);
 
         Destroy(player.gameObject);
@@ -440,8 +439,7 @@ public class MDPLevelController : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         Instantiate(winOverlay, transform, true);
     }
-    IEnumerator Lost()
-    {
+    IEnumerator Lost() {
         Vector3 position = player.transform.localPosition;
 
         Destroy(player.gameObject);
@@ -452,35 +450,32 @@ public class MDPLevelController : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         Instantiate(loseOverlay, transform, true);
     }
-    bool FallHoles()
-    {
+    bool FallHoles() {
         Vector3 playerPosistion = player.transform.localPosition;
-        int x = (int)playerPosistion.x;
-        int y = (int)playerPosistion.y;
-        return hole[x, y] == 1 ? true : false;
+        int x = (int) playerPosistion.x;
+        int y = (int) playerPosistion.y;
+        return hole[x,y] == 1 ? true : false;
     }
-    bool Blocked(Vector3 position, Vector3 direction)
-    {
+    bool Blocked(Vector3 position, Vector3 direction) {
         int x = (int)position.x;
         int y = (int)position.y;
-        int n = size - 1;
-
+        int n = size-1;
+        
         if (direction == Vector3.up)
-            return y == n || horizontalWall[x, y + 1] == 1;
+            return y == n || horizontalWall[x, y+1] == 1;
 
         if (direction == Vector3.down)
             return y == 0 || horizontalWall[x, y] == 1;
 
         if (direction == Vector3.left)
             return x == 0 || verticalWall[x, y] == 1;
-
+        
         if (direction == Vector3.right)
-            return x == n || verticalWall[x + 1, y] == 1;
-
+            return x == n || verticalWall[x+1, y] == 1;
+        
         return true;
     }
-    IEnumerator RunEffect(GameObject effect, Vector3 position, bool clear)
-    {
+    IEnumerator RunEffect(GameObject effect, Vector3 position, bool clear) {
         GameObject fx = Instantiate(effect, transform);
         fx.transform.localPosition = position;
         yield return fx.GetComponent<Effect>().Run(clear);
